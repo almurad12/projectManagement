8dbbe3100afe91b12f8ac62e591af137397f6982
unknown <murad.ahmed061@gmail.com>
2025-03-04 14:31:31 +0600

    requirment add

--- projectManagement/projectManagement/settings.py
+++ projectManagement/projectManagement/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
+    'drf_yasg',
     'account',
     'project',
     'rest_framework_simplejwt',
--- projectManagement/projectManagement/urls.py
+++ projectManagement/projectManagement/urls.py
@@ -16,10 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import permissions
 from account import urls
+# ###swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+
+# Define schema view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Project Management System",
+        default_version='v1',
+        description="API documentation for My Project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/',include('account.urls')),
     path('api/projects/',include('project.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
